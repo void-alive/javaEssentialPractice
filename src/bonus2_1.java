b4e7792d9bdd86f0083192d252b21d1cf036ba5d
void-alive <sja9284@gmail.com>
2024-12-02 15:42:00 +0900

    보너스2번 문제 주석 일부 추가

--- src/bonus2_1.java
+++ src/bonus2_1.java
@@ -7,8 +7,12 @@ public class bonus2_1 {
 //    배열의 처음부터 마지막 index까지 반복
     for(int i=0;i<numbers.length;i++){
       if(i<numbers.length-1){
-//        맨 끝의 한칸 남기기
+//        i+1인 index가 배열의 마지막 index를 넘지 못하도록 함
+//        배열.length-1은 해당 배열의 마지막 index가 됨
+//        i가 마지막 index까지 못가고, i+1이 마지막 index가 되도록 함
         if(numbers[i]>numbers[i+1]){
+//          i index인 값과 i+1 index인 값을 비교하여 i의 값이 더 큰 경우
+//          변수에 저장
           big = numbers[i];
         }
       }
