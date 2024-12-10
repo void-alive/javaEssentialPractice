293d3e90a33e9db5325227fb17b3a64ecc84b8ab
void-alive <sja9284@gmail.com>
2024-12-10 14:20:41 +0900

    문제 몇 개 더 추가1

--- book_4/Car.java
+++ book_4/Car.java
@@ -2,7 +2,6 @@ public class Car {
   private String brand;
   private String color;
   private int year;
-  private final String drive = "자동차가 달립니다.";
 
 //  문제 1
 //  Car() {
@@ -13,13 +12,13 @@ public class Car {
 
 //  문제 2
 //  void : 단순히 출력만 해줌 (결과값 return 필요 x)
-//  그 외 나머지 연산자 : 값을 바꾸거나 할 때 사용
-//  return 필요
+//  그 외 나머지 연산자 : 값을 바꾸거나 할 때 사용. return 필요
   public void drive() {
-    System.out.println(drive);
+    System.out.println("차달려요");
   }
 
 //  문제 3
+//  매개변수 명과 필드 명이 같을 경우 this. 붙여서 정확히
   Car(String brand, String color, int year) {
     this.brand = brand;
     this.color = color;
@@ -27,7 +26,8 @@ public class Car {
   }
 
 //  문제 4
-  //  getter : 값 읽기
+//  getter : 값 읽기
+//  호출한 곳으로 되돌려주기 위해 return 필요
   public String getBrand(){
     return brand;
   }
@@ -38,7 +38,8 @@ public class Car {
     return year;
   }
 
-//  setter : 값 바꾸기
+  //  setter : 값 바꾸기
+  //  값을 저장하기 위한 것이라 return 필요 x -> void
   public void setBrand(String brand){
     this.brand = brand;
   }
@@ -49,6 +50,7 @@ public class Car {
     this.year = year;
   }
 
+
 //  문제 5
   public void printInfo(){
     System.out.println("브랜드 : " + brand);
@@ -58,9 +60,9 @@ public class Car {
 
 //  문제 6
   Car(){
-    brand = "미정";
-    color = "검정";
-    year = 2000;
+    this.brand = "미정";
+    this.color = "검정";
+    this.year = 2000;
   }
 
   public static void main(String[] args) {
@@ -74,7 +76,7 @@ public class Car {
 
 //    문제 2
     System.out.println("문제 2");
-    System.out.println(car.drive);
+    car.drive();
     System.out.println();
 
 //    문제 3
@@ -95,16 +97,17 @@ public class Car {
     System.out.println();
 //    바꾼 뒤
 //    원래 private이라서 바꿀수 없는 값들이지만
-//    public 속성을 가진 getter와 setter를 사용해서
-//    setter 값 바꿈 = private 값을 바꾸는 것과 같은 효과
-//    간접적으로 수정해줌
-//    getter : 간접적으로 수정한 값을 읽어옴
+//    public 속성을 가진 getter와 setter를 사용해서 간접적으로 수정
+//    setter : 값을 수정함
+//    getter : 수정한 값을 읽어옴
+//    같은 클래스 내부에서는 private 접근 가능
+//    제대로 실습하고 싶다면 다른 클래스를 만들어서 해보는 것추천
     car2.setBrand("가나다");
     car2.setColor("빨강");
     car2.setYear(2020);
-    System.out.println(car2.brand);
-    System.out.println(car2.color);
-    System.out.println(car2.year);
+    System.out.println(car2.getBrand());
+    System.out.println(car2.getColor());
+    System.out.println(car2.getYear());
     System.out.println();
 
 //    문제 5
@@ -125,13 +128,10 @@ public class Car {
     System.out.println("문제 7");
 //    car5를 5개의 배열로 정의
     Car car5 [] = new Car[5];
-//    레퍼런스 변수 (객체 이름) 선언 -> 아직 배열 생성 x
-    Car car6 [];
-//    5개의 칸을 가진 배열 생성, 그후 car6에 넣음
-    car6 = new Car[5];
 
 //    car5.length(5)까지 5번 반복함
     for(int i=0;i<car5.length;i++){
+//      객체 생성
       car5[i] = new Car( i + "번",i + "색",i);
     }
 //    car5.length(5)까지 5번 출력함
@@ -139,6 +139,7 @@ public class Car {
       System.out.print(car5[i].brand + " ");
       System.out.print(car5[i].color + " ");
       System.out.println(car5[i].year);
+//      car5[i].printInfo();도 가능
     }
     System.out.println();
   }
--- book_4/Lamp.java
+++ book_4/Lamp.java
@@ -3,13 +3,13 @@ public class Lamp {
   private boolean inOn;
 
   //  turnOn 메서드
-  //  inOn을 true 값으로 만들고 리턴한다
+  //  inOn을 true 값으로 만든다
   public void turnOn() {
     inOn = true;
   }
 
   //  turnOff 메서드
-  //  inOn을 false 값으로 만들고 리턴한다
+  //  inOn을 false 값으로 만든다
   public void turnOff() {
     inOn = false;
   }
@@ -17,8 +17,9 @@ public class Lamp {
   //  printStatus() 메소드
   //  inOn이 true인지 false인지 확인하고 값을 출력한다
   //  단순히 출력만 하면 되니까 void로 지정
+  //  반환한다 : return
   public void printStatus() {
-    if (inOn == true) {
+    if (inOn==true) {
       System.out.println("램프는 켜져있습니다.");
     } else {
       System.out.println("램프는 꺼져있습니다.");
