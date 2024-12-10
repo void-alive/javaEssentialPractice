aaeec381cbc054ad4a94ef82691a9d4720f23e28
void-alive <sja9284@gmail.com>
2024-12-10 09:50:33 +0900

    선생님의 해설을 주석으로 추가3

--- book_4/Box.java
+++ book_4/Box.java
@@ -1,13 +1,26 @@
 public class Box {
+//  필드
+//  width, height를 final로 만들면 : 수정 불가
   private int width, height;
   private char fillChar;
+
+//  생성자
+//  Box()의 기본값 : 가로 10, 세로 1
+//  this. : 객체 자신의 멤버 호출
+//  this() : 생성자가 다른 생성자 호출
   public Box(){
     this(10,1);
   }
+
+//  private인 width, height값을 public으로 잠깐 받기
+//  매개변수로 너비와 높이를 가진 생성자
+//  this(10,1)로 인해 호출됨
   public Box(int width, int height){
     this.width = width;
     this.height = height;
   }
+
+//  fillchar에 있는 문자를 width와 height만큼 그리기
   public void draw(){
     for(int i=0;i<height;i++){
       for(int j=0;j<width;j++){
@@ -16,9 +29,13 @@ public class Box {
       System.out.println();
     }
   }
+
+//  char 데이터형인 c를 fillchar 필드(변수)에 넣기
+//  필드(변수)의 setter -> 쓰기
   public void fill(char c){
     this.fillChar = c;
   }
+
   public static void main(String[] args){
     Box a=new Box();
     Box b=new Box(20,3);
--- book_4/StaticTest.java
+++ book_4/StaticTest.java
@@ -1,48 +1,69 @@
 // default 클래스 : 다른 패키지 안에 있으면 사용 불가
 class Circle {
+//  private 접근자를 사용하는 필드(변수). 반지름 저장
   private int radius;
 
+//  매개변수 하나를 가지는 생성자. 필드(변수) radius의 값을 초기화(대입)할 때 사용
   public Circle(int radius) {
     this.radius = radius;
   }
 
+//  private으로 선언된 radius 변수를 public으로 읽어옴 (간접적으로 확인 가능)
+//  필드(변수) radius의 getter -> 읽기
   public int getRadius() {
     return this.radius;
   }
 
+//  private으로 선언된 radius 변수를 public으로 수정 (간접적 수정)
+//  필드(변수) radius의 setter -> 쓰기
   public void setRadius(int radius) {
     this.radius = radius;
   }
 }
 
 class CircleManager {
+//  클래스랑 같은 메모리 공간에 있음. 클래스가 생성될 때 생성되고, 소멸될 때 같이 소멸됨
+//  이름은 스태틱. 스태틱이 아닌 건 인스턴스 변수라고 함 (스태틱 외에 다른 평범한 메소드들)
+//  src를 dest에 복사
+//  정적 멤버. 매개변수로 Circle 클래스 타입의 객체 2개 사용
   static void copy(Circle src, Circle dest) {
+//    원본의 반지름 값을 받아서 사본에 저장
     dest.setRadius(src.getRadius());
   }
 
+//  a의 반지름과 b의 반지름이 같은지 확인.
   static boolean equals(Circle a, Circle b) {
     if (a.getRadius() == b.getRadius()) {
       return true;
     } else {
       return false;
     }
+//    단순화
+//    result a.retRadius() == b.getRadius();
   }
 }
 
 public class StaticTest {
   public static void main(String[] args) {
+//    클래스에 객체 이름 (레퍼런스) 선언
     Circle pizza = new Circle(5);
     Circle waffle = new Circle(1);
 
+//    pizza랑 waffle이 같은지 확인
     boolean result = pizza.equals(waffle);
+//    boolean result = CircleManager.equals(pizza, waffle);
+
     if (result == true)
+//      if (result) 단순화
       System.out.println("pizza와 waffle 크기 같음");
     else
       System.out.println("pizza와 waffle 크기 다름");
 
+//    pizza를 waffle에 복사
     CircleManager.copy(pizza, waffle);
     result = CircleManager.equals(pizza, waffle);
     if (result == true)
+//      if (result) 단순화
       System.out.println("pizza와 waffle 크기 같음");
     else
       System.out.println("pizza와 waffle 크기 다름");
